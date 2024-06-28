61f0d2e89e4615c38bd02b0d1317cb0b3034bed1
suhila77 <suhilab9@gmail.com>
2024-06-28 16:09:28 -0700

    task4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/user/bin/python3
+"""  Your Comments Here """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    c = db.cursor()
+    c.execute("""SELECT cities.id, cities.name, states.name, FROM
+                  cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
